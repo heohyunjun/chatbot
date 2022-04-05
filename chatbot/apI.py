c0486b8322221f338b67d028aa2c15b923ceb088
heohyunjun <hhj2676@naver.com>
2022-04-05 23:41:01 +0900

    feat : dataset loading using cache
    error :  streamlit 설치 에러

--- chatbot/apI.py
+++ chatbot/apI.py
@@ -8,8 +8,19 @@ import json
 '''
 모델 로드를 사용할떄마다 계속 부르게 되면 latency가 매우 길어짐
 따라서 한번 부르고 cache 형태로 불러옴
+
+모델과 동일하게 데이터셋도 캐싱함
 '''
 @st.cache(allow_output_mutation = True)
 def cached_model():
     model = SentenceTransformer('/model')
     return model
+
+@st.cache(allow_output_mutation = True)
+def get_dataset():
+    df = pd.read_csv('chat_text.csv')
+    df['embedding'] = df['embedding'].apply(json.loads)
+    return df
+
+model = cached_model()
+df = get_dataset()
