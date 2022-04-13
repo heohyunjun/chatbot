3b3596aeba262be34fedc7b8c465e664ff0f83ba
heohyunjun <hhj2676@naver.com>
2022-04-14 00:36:39 +0900

    feat : netflix_rec_submitted

--- chatbot/apI.py
+++ chatbot/apI.py
@@ -4,7 +4,8 @@ import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 '''
 
 No streamlit error in python 3.9
@@ -35,8 +36,23 @@ def get_dataset():
     df['embedding'] = df['embedding'].apply(json.loads)
     return df
 
+@st.cache(allow_output_mutation = True)
+def get_netflix():
+    df2 = pd.read_csv('net_rec.csv')
+    return df2
+
 model = cached_model()
 df = get_dataset()
+df2 = get_netflix()
+
+
+# 불용어 제거
+tfidf = TfidfVectorizer(stop_words='english')
+# 영화설명 컬럼 학습
+tfidf_matrix = tfidf.fit_transform(df2['description'])
+# linear_kernel : 사이킷런에서 제공하는 문서 유사도(코사인 유사도)를 계산 할 수 있음
+cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
 
 st.header('chat bot')
 
@@ -73,8 +89,15 @@ if user_input:
         # 챗봇 답변 저장
         st.session_state.generated.append(answer['챗봇'])
     elif netflix_submitted:
+        print(user_input)
+        idx = df2[df2['title'] == user_input].index[0]
+        sim_scores = list(enumerate(cosine_sim[idx]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-        answer = '영화 추천버튼이 클릭되었습니다.'
+        movie_idx = sim_scores[1][0]
+        title = df2['title'].iloc[movie_idx]
+        # answer = '영화 추천버튼이 클릭되었습니다.'
+        answer = title + "를 추천합니다"
         st.session_state.generated.append(answer)
     else:
         pass
